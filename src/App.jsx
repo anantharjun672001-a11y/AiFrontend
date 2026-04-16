15d451c8a042593ee3cb83d64f3f7b383fb8fc94
anantharjun672001-a11y <anantharjun672001@gmail.com>
2026-04-16 15:30:28 +0530

    sample ai testing successfull ai code review working

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,51 @@
-import React from 'react';
+import { useState } from "react";
+import axios from "axios";
 
-const App = () => {
+function App() {
+  const [code, setCode] = useState("");
+  const [result, setResult] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const analyzeCode = async () => {
+    try {
+      setLoading(true);
+
+      const res = await axios.post("http://localhost:3000/analyze", {
+        code,
+      });
+
+      setResult(res.data.result);
+    } catch (err) {
+      console.error(err);
+      setResult("Error analyzing code");
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
-    <div>
-      
+    <div style={{ padding: "20px" }}>
+      <h2>AI Code Reviewer </h2>
+
+      <textarea
+        rows="10"
+        cols="60"
+        placeholder="Paste your code here..."
+        value={code}
+        onChange={(e) => setCode(e.target.value)}
+      />
+
+      <br /><br />
+
+      <button onClick={analyzeCode}>
+        {loading ? "Analyzing..." : "Analyze"}
+      </button>
+
+      <pre style={{ marginTop: "20px", whiteSpace: "pre-wrap" }}>
+        {result}
+      </pre>
     </div>
   );
-};
+}
 
 export default App;
